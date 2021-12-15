53a6711fe7e66dba5384089268ba4f531f755e4d
Yannick-N <yannick.napoleon@epitech.eu>
2021-12-15 16:35:45 +0100

    [✨] add routing system

--- tourOfHeroes/src/app/hero.service.ts
+++ tourOfHeroes/src/app/hero.service.ts
@@ -2,16 +2,17 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './heroes-db';
 import { Observable, of } from 'rxjs'
-
+import { MessagesService } from './messages.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messagesService: MessagesService) { }
 
   getHeroes(): Observable<Hero[]> {
+    this.messagesService.add(`HeroService: Fetched Heroes`);
     return of(HEROES);
   }
 }
